29e9c5cd6927d1bccec2cdfb5095eeb113f66188
domingueslais <domingueslais@users.noreply.github.com>
2023-02-23 13:55:06 -0300

    Completo

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 //alert("Evento apenas para MAIORES DE 18 anos!")
 
-function inscricao() {
+function envio() {
+    //alert("OK")
+   var data = new Date()
+   var ano = data.getFullYear()
+   var nasc = document.getElementById('iano')
+   var idade = ano - Number(nasc.value)
+   let nome = document.getElementById('inome')
+   let curso = document.getElementById('icurso')
 
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.getElementById('iano')
-    var resposta = document.getElementById('resposta')
-    var idade
+   if (nome.value.length == 0 || curso.value.length != 3 || ano < nasc.value || nasc.value < 1950) {
+    alert('Preencha todos os campos corretamente')
+   }
+   else {
+    alert("Dados recebidos com sucesso!")
+   }
 
-    if (Number=(fano.value)>ano){
-        alert("ERRO. Ano inválido.")
-    }
-    else if {
-        idade = ano - Number(fano.value)
-        resposta.innerHTML = `Sua idade é ${idade}, para participar, é necessário ter 18 anos ou mais.`
+   if (idade.value<18) {
+    resposta.innerHTML = `Infelizmente você não tem a idade mínima para participar do trote. Sentimos muito.`
+   }
+   else {
+    resposta.innerHTML = `Olá, ${nome}, calouro de ${curso}. Recebemos sua inscrição e as coisas que você topa e não topa fazer. Torcemos para que você tenha um excelente trote. Nos vemos no dia 8.`+ <br></br>
+    `Seja bem vindo(a).`
     }
 }
